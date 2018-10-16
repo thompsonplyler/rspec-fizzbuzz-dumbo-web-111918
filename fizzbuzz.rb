d0b049191ee35b0affd5d04034676e0765584b25
Thompson Plyler <nealok.g@gmail.com>
2018-10-16 18:27:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz 
-  
+  if int % 3 == 0 
+    "Fizz"
+  elsif int % 5 == 0
+  "Buzz"
+  elsif (int % 5 == 0) && (int % 3 == 0)
+  "Fizzbuzz"
+else 
+  nil
+end
 end
